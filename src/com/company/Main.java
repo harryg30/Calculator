8a25fcbcfb2588b064f76c095629740026bd0b3d
harryg30 <gordenstein30@gmail.com>
2017-03-22 17:58:08 -0400

    buttons are functioning but no operations preformed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,6 +18,9 @@ import java.util.List;
 public class Main extends Application{
 
     private TextField textField = new TextField();
+    static ArrayList<Integer> nums = new ArrayList<Integer>(); //individual button presses
+    static ArrayList<Double> fullNums = new ArrayList<Double>(); //after user input is put together
+    static ArrayList<String> operations = new ArrayList<String>();//operations
 
     @Override
     public void start(Stage primarystage) throws Exception {
@@ -39,7 +42,7 @@ public class Main extends Application{
             Button b = new Button(button);
             b.setMinSize(80,80);
             pane.getChildren().add(b);
-            //b.setOnAction(e -> doSomething(b.getText()));
+            b.setOnAction(e -> doSomething(b.getText()));
         }
 
         Scene scene = new Scene(pane,536,500);
@@ -50,4 +53,29 @@ public class Main extends Application{
     public static void main(String[] args) {
         Application.launch(args);
     }
+
+    public static void doSomething(String s){
+
+        if(s.equals("0")||s.equals("1")||s.equals("2")||s.equals("3")||s.equals("4")||s.equals("5")||s.equals("6")||s.equals("7")||
+                s.equals("8")||s.equals("9")){
+           nums.add(Integer.parseInt(s));
+        }
+        else if(s.equals("+")||s.equals("-")||s.equals("/")||s.equals("X")){
+            fullNums.add(retNums(nums));
+            nums.clear();
+        }
+        else if(s.equals("=")){
+
+        }
+     }
+     //combuines all button presses up till operation into one number
+     public static double retNums(ArrayList<Integer> digits){
+        double ret = 0;
+        int place = 0;
+        for(int i=digits.size()-1; i>=0; i--){
+            ret += digits.get(i)*Math.pow(10,place);
+            place++;
+        }
+        return ret;
+     }
 }
